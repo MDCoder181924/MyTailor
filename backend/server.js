7455ba06d3f2b9272c3e749464802ecec9fecc36
MDCoder181924 <dobariyam181924@gmail.com>
2026-05-14 22:47:45 +0530

    route define

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ import app from "./src/app.js";
 
 dotenv.config();
 
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
 // Connect to DB when the module is initialized. In serverless environments
 // (Vercel) we export the Express `app` as the default export so Vercel can
 // invoke it as a function. Do NOT call `app.listen()` here.
